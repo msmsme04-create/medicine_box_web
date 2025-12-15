97aab98af393e6fbce5e4cf0c2073361e1f9df85
Minseo Kim <msmsme04@inu.ac.kr>
2025-12-16 00:42:10 +0900

    데이터베이스 생성 수정

--- create_db.py
+++ create_db.py
@@ -5,12 +5,24 @@ conn = sqlite3.connect("medicine.db")
 conn.execute("""
 CREATE TABLE IF NOT EXISTS box (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    csv_no INTEGER,
+    district TEXT,
     name TEXT,
-    address TEXT
+    address TEXT,
+    detail TEXT
+)
+""")
+
+conn.execute("""
+CREATE TABLE IF NOT EXISTS report (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    box_id INTEGER,
+    content TEXT,
+    created_at TEXT
 )
 """)
 
 conn.commit()
 conn.close()
 
-print("DB 생성 완료!")
+print("DB 생성 완료")
